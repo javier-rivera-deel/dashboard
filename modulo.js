26c9e4fe694c83f40b65b23b41d2927f371fa0e6
jrivera-technisys <jrivera@technisys.com>
2016-12-06 17:37:21 -0300

    still working on populating select options with mustache

--- modulo.js
+++ modulo.js
@@ -1,17 +1,23 @@
-(function () {
+(function(){
     var dashboard = {
+        categories : subArray(respuesta),
         init: function () {
-
+            //sortData(respuesta);
+            this.cacheElements();
+            this.render();
         },
-        cacheDom: function () {
-
+        cacheElements: function () {
+            this.$category = document.getElementById('category');
+            this.$totalAmaount = document.getElementById('totalAmaount');
+            this.$select = $('#categories-select');
+            this.template = $('#categories-list').html();
         },
-        render: function () {
-
-        },
-        bindEvents: function () {
-
+        render:function(){
+            var data = {
+                categories : this.categories
+            };
+            this.$select.html(Mustache.render(this.template,data)); 
         }
-    };
+    }; 
     dashboard.init();
 })();
