c2a9a3da0e551ae39c76ed1681da6a50b4b70f4a
jrivera-technisys <jrivera@technisys.com>
2016-12-13 17:05:32 -0300

    graph animated

--- graph.js
+++ graph.js
@@ -95,6 +95,19 @@ var graph = {
                 },*/
             } /*PADDING*/
         });
+        chart.on("draw", function(data) {
+                if(data.type === "line" || data.type === "area") {
+                    data.element.animate({
+                        d: {
+                            begin: 2000 * data.index,
+                            dur: 4000,
+                            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+                            to: data.path.clone().stringify(),
+                            easing: Chartist.Svg.Easing.easeOutQuint
+                        }
+                    });
+                }
+            });
     }
    };
 
